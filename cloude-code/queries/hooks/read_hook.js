cae8984e9d1be9602ef3566845c824d5a1c821f4
Luciano Baraúna <lucianobaraunadev@gmail.com>
2026-05-27 09:20:07 -0300

    curso finalizado

--- cloude-code/queries/hooks/read_hook.js
+++ cloude-code/queries/hooks/read_hook.js
@@ -1,15 +1,16 @@
 async function main() {
-  const chunks = [];
-  for await (const chunk of process.stdin) {
-    chunks.push(chunk);
-  }
-  const toolArgs = JSON.parse(Buffer.concat(chunks).toString());
-
-  // readPath is the path to the file that Claude is trying to read
-  const readPath =
-    toolArgs.tool_input?.file_path || toolArgs.tool_input?.path || "";
-
-  // TODO: ensure Claude isn't trying to read the .env file
+  process.stdin.setEncoding("utf8");
+  let input = "";
+  process.stdin.on("data", (d) => (input += d));
+  process.stdin.on("end", () => {
+    const toolArgs = JSON.parse(input);
+    const readPath = toolArgs.tool_input?.file_path || "";
+    if (readPath.includes(".env")) {
+      console.error("You cannot read the .env file");
+      process.exit(2);
+    }
+    process.exit(0);
+  });
 }
 
 main();
